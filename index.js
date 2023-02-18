5cb3befdf2a9c00e438811d05501ff6754357ebc
Franco Castany <francocastany@gmail.com>
2023-02-18 13:54:18 -0300

    finished

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import './style.css';
-
 const passwordInput = document.querySelector('.passwordInput');
 const generateBtn = document.querySelector('.generateBtn');
 const rangeInput = document.querySelector('.rangeInput');
@@ -11,27 +8,25 @@ const copyBtn = document.querySelector('.copy-icon');
 const char = 'abcdefghijklmnñopqrstuvwxyz123456789.,-_{}[]()<>=+*/#$%&';
 
 const generatePsw = () => {
+  if(copyBtn.classList.contains('bi-clipboard-check')) copyBtn.classList.replace('bi-clipboard-check', 'bi-clipboard')
   let psw = '';
   let pswLength = rangeInput.value;
 
-  console.log(pswLength);
   for (let i = 0; i < pswLength; i++) {
     psw += char[Math.round(Math.random() * char.length)];
   }
 
-  console.log(psw);
-
   passwordInput.value = psw;
-};
-
-generateBtn.addEventListener('click', () => {
-  generatePsw();
 
   copyBtn.addEventListener('click', () => {
     navigator.clipboard.writeText(passwordInput.value);
     console.log(`Copied: ${psw}`);
     copyBtn.classList.replace('bi-clipboard', 'bi-clipboard-check');
   });
+};
+
+generateBtn.addEventListener('click', () => {
+  generatePsw();
 });
 
 rangeInput.addEventListener('change', () => {
